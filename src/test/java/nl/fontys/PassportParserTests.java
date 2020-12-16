db198f1d6e73460265f41f0686d608e23255cc3e
Timo Hermans <tmhermans@gmail.com>
2020-12-16 19:02:17 +0100

    Make it possible to run tests

---
+++ src/test/java/nl/fontys/PassportParserTests.java
@@ -0,0 +1,14 @@
+package nl.fontys;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PassportParserTests {
+    @Test
+    public void Can_parse_a_valid_passport() {
+        int x = 10;
+
+        assertEquals(10, x);
+    }
+}
