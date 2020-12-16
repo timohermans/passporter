4592a7e2863d12ad33514e1b89ab950113a15781
Timo Hermans <tmhermans@gmail.com>
2020-12-16 19:48:51 +0100

    Remove dead code from unit test

--- src/test/java/nl/fontys/PassportParserTests.java
+++ src/test/java/nl/fontys/PassportParserTests.java
@@ -7,16 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PassportParserTests {
     @Test
     public void Can_parse_a_valid_passport() {
-        // arrange
         var passportString = """
 ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm""";
         var parser = new PassportParser();
 
-        // act
         var result = parser.parse(passportString);
 
-        // assert
         assertEquals(new Passport("gry", 860033327, 2020, "#fffffd", 1937, 2017, 147, "183cm"), result);
     }
 }
